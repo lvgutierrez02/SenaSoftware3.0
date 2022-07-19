b08efcda2db738256dbd2cb5e4cc714c5f306a26
Laura Gutierrez <lvgutierrez210@hotmail.com>
2022-07-19 16:49:22 -0500

    GET

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -11,8 +11,10 @@ namespace Repository
 {
     public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
     {
-        public CompanyRepository(RepositoryContext repositoryContext) : base(repositoryContext)
+        public CompanyRepository(RepositoryContext repositoryContext)
+            : base(repositoryContext)
         {
         }
+        public IEnumerable<Company> GetAllCompanies(bool trackChanges) => FindAll(trackChanges).OrderBy(c => c.Name).ToList();
     }
 }
